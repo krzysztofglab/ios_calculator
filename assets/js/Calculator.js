2992604e0bbf3f3556e2a7ded97830f3b15d18f8
Krzysztof Głąb <krzysztofglab.98@gmail.com>
2023-01-27 23:25:12 +0100

    Code optimisation and working on the correct calculation

--- assets/js/Calculator.js
+++ assets/js/Calculator.js
@@ -1,183 +1,194 @@
 export class Calculator {
   constructor() {
-    this.output = '0';
-    this.currentValue = '0';
-    this.previousValue = null;
-    this.currentOperator = null;
-    this.numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
-    this.elResult = document.querySelector('.result');
-    this.elClear = document.querySelector('.btn-clear');
-    this.elButtons = document.querySelectorAll('button');
+    this.initValues();
+    this.initElements();
     this.initEventListeners();
   }
 
+  initValues() {
+    this.val1 = null;
+    this.val2 = null;
+    this.result = null;
+    this.operator = null;
+    this.inputValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
+    this.output = '0';
+  }
+
+  initElements() {
+    this.UI = {
+      "result": document.querySelector('.result'),
+      "clear": document.querySelector('.btn-clear'),
+      "buttons": document.querySelectorAll('button'),
+    }
+  }
+
+  initEventListeners() {
+    this.UI.buttons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        this.action(e.target.innerText, button);
+      });
+    });
+  }
+
   action(value, button) {
-    const key = this.numbers.includes(value) ? "number" : value;
-    this.elButtons.forEach(button => button.classList.remove('active'));
+    const key = this.inputValues.includes(value) ? "input" : value;
+    //Clear CSS active class for operator buttons
+    this.UI.buttons.forEach(button => button.classList.remove('active'));
 
     switch (key) {
       case "AC":
-        this.clearResult();
-        this.writeResult();
+        this.clearOutput();
       break;
       case "C":
-        this.clearResult();
-        this.writeResult();
+        this.clearOutput();
       break;
       case "±":
-        if(this.currentValue.includes('-')) {
-          this.currentValue = this.currentValue.replace('-', '');
-        } else {
-          this.currentValue = `-${this.currentValue}`;
-        }
-        this.writeResult();
+        this.nagateOutput();
       break;
       case "%":
+        this.getOutputPercentage();
       break;
       case "÷":
-        this.setOperator('/');
-        button.classList.add('active');
+        this.setOperator('/', button);
       break;
       case "×":
-        this.setOperator('*');
-        button.classList.add('active');
+        this.setOperator('*', button);
       break;
       case "-":
-        this.setOperator('-');
-        button.classList.add('active');
+        this.setOperator('-', button);
       break;
       case "+":
-        this.setOperator('+');
-        button.classList.add('active');
+        this.setOperator('+', button);
       break;
       case "=":
-        this.calculate();
       break;
-      case "number":
-        this.updateResult(value);
+      case "input":
+        this.updateOutput(value);
       break;
       default:
         break;
     }
   }
 
-  clearResult() {
-    this.currentValue = "0"
-    this.previousValue = '';
-    this.currentOperator = '';
+  clearOutput() {
+    this.val1 = null;
+    this.val2 = null;
+    this.result = null;
+    this.operator = null;
+    this.output = '0';
+    this.writeResult();
   }
 
-  calculate() {
-    if(this.currentOperator == '') return
-    if(this.currentValue == '') {
-      this.currentValue = eval(`${Number(this.previousValue.replace(',','.'))} ${this.currentOperator} ${Number(this.previousValue.replace(',','.'))}`);
+  getOutputPercentage() {
+    const value = Number(this.output.replace(',','.'));
+    const valuePercentage = (value / 100);
+    this.output = valuePercentage.toString().replace('.',',');
+    this.writeResult();
+  }
+
+  nagateOutput() {
+    const isNegativeNumber = this.output.includes('-');
+    isNegativeNumber ? this.output = this.output.replace('-', '') : this.output = `-${this.output}`;
+    this.writeResult();
+  }
+
+  setOperator(operator, button) {
+    button.classList.add('active');
+    
+    if(this.val1 == null) {
+      this.val1 = this.output;
+      this.writeResult();
+      this.output = '0';
+    } else if(this.val2 == null) {
+      this.val1 = eval(`${this.val1} ${this.operator} ${this.output}`);
+      this.output = this.val1.toString();
+      this.writeResult();
+      this.output = '0';
     } else {
-      this.currentValue = eval(`${Number(this.previousValue.replace(',','.'))} ${this.currentOperator} ${Number(this.currentValue.replace(',','.'))}`);
-    }
-    this.validateResult();
-    this.previousValue = this.currentValue;
-    this.currentValue = '';
-  }
-
-  validateResult() {
-    const valueLength = this.currentValue.toString().length > 9 ? 7 : this.currentValue.toString().length - 2;
-    if(this.currentValue < 1) {
-      this.currentValue = this.currentValue.toFixed(valueLength).toString().replace('.',',');
-      this.writeResult(this.currentValue);
-    } else {
-      this.currentValue = this.currentValue.toString().replace('.',',');
-      this.writeResult(this.currentValue);
+      this.val1 = this.output;
+      this.val2 = null;
+      this.writeResult();
+      this.output = '0';
     }
 
+    this.operator = operator;
+    console.log('Operator set to: ' + operator);
+    console.log(`${this.val1} ${operator} ${this.val2}`)
   }
 
-  setOperator(operator) {
-    if(this.previousValue != '' && this.currentValue != '') {
-      this.calculate();
-    } else {
-      if(this.currentValue != '') {
-        this.previousValue = this.currentValue;
-        this.currentValue = '';
-      }
-    }
+  updateOutput(value) {
+    const valueLengthLimit = 9;
+    const valueLength = this.output.replace('-','').replace(',','').length;
 
-    this.currentOperator = operator;
-  }
-
-  updateResult(value) {
-    const valueLength = this.currentValue.replace('-','').replace(',','').length;
-
-    if(valueLength < 9) {
+    if(valueLength < valueLengthLimit) {
       if(value == "0") {
-        if(this.currentValue != "0" && this.currentValue != "-0") {
-          this.currentValue += value;
+        if(this.output != "0" && this.output != "-0") {
+          this.output += value;
         }
       } else {
-        if(this.currentValue == "0" || this.currentValue == "-0") {
+        if(this.output == "0" || this.output == "-0") {
           if(value == ",") {
-            this.currentValue = `${this.currentValue},`;
+            this.output = `${this.output},`;
           } else {
-            if(this.currentValue == "0" ) {
-              this.currentValue = value;
+            if(this.output == "0" ) {
+              this.output = value;
             } else {
-              this.currentValue = `-${value}`;
+              this.output = `-${value}`;
             }
           }
         } else {
           if(value == ",") {
-            if(!this.currentValue.includes(",")) {
-              this.currentValue += value;
+            if(!this.output.includes(",")) {
+              this.output += value;
             }
           } else {
-            this.currentValue += value;
+            this.output += value;
           }
         }  
       }
       this.writeResult();
+    } else {
+      console.log('Value lenght limit (9) exceeded!');
     }
   }
 
   writeResult() {
-    this.currentValue == "0" || this.currentValue == "-0" ? this.elClear.textContent = "AC" : this.elClear.textContent = "C";
+    this.output == "0" || this.output == "-0" ? this.UI.clear.textContent = "AC" : this.UI.clear.textContent = "C";
 
-    const isComma = this.currentValue.includes(',');
-    const value0 = this.currentValue.split(',')[0];
-    let array = value0.split("").reverse();
-    let result = this.currentValue;
+    const isComma = this.output.includes(',');
+    const intiger = this.output.split(',')[0];
+    let array = intiger.split("").reverse();
+    let result = this.output;
 
-    if(value0.length > 6) {
+    if(intiger.length > 6) {
       array.splice(3,0," ");
       array.splice(7,0," ");
       result = array.reverse().join("");
-      isComma ? result = `${result},${this.currentValue.split(',')[1]}`: '';
+      isComma ? result = `${result},${this.output.split(',')[1]}`: '';
 
-      this.elResult.textContent = result;
-    } else if(value0.length > 3) {
+      this.UI.result.textContent = result;
+    } else if(intiger.length > 3) {
       array.splice(3,0," ");
       result = array.reverse().join("");
-      isComma ? result = `${result},${this.currentValue.split(',')[1]}`: '';
+      isComma ? result = `${result},${this.output.split(',')[1]}`: '';
 
-      this.elResult.textContent = result;
+      this.UI.result.textContent = result;
     } else {
-      this.elResult.textContent = result;
+      this.UI.result.textContent = result;
     }
 
-    // Adjust font-size if overflows container
+    this.adjustFontSize();
+
+  }
+
+  adjustFontSize() {
     let decreasePixels = 0;
-    this.elResult.style.fontSize = `${85-decreasePixels}px`;
+    this.UI.result.style.fontSize = `${85-decreasePixels}px`;
     for (let i = 0; i < 40; i++) {
-      if(this.elResult.offsetWidth > 360) {
-        this.elResult.style.fontSize = `${85-decreasePixels}px`;
+      if(this.UI.result.offsetWidth > 360) {
+        this.UI.result.style.fontSize = `${85-decreasePixels}px`;
         decreasePixels += 1;
       }
     }
-  }
-
-  initEventListeners() {
-    this.elButtons.forEach(button => {
-      button.addEventListener('click', (e) => {
-        this.action(e.target.innerText, button);
-      });
-    });
   }
 }
