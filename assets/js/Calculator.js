c7c34aab83797865da5ff6a966fb0ea5f00e2de1
Krzysztof Głąb <krzysztofglab.98@gmail.com>
2023-01-28 13:58:20 +0100

    Improve calculation logic

--- assets/js/Calculator.js
+++ assets/js/Calculator.js
@@ -61,6 +61,7 @@ export class Calculator {
         this.setOperator('+', button);
       break;
       case "=":
+        this.calculate();
       break;
       case "input":
         this.updateOutput(value);
@@ -87,9 +88,34 @@ export class Calculator {
   }
 
   nagateOutput() {
+    this.output == null ? this.output = this.val1 : '';
     const isNegativeNumber = this.output.includes('-');
     isNegativeNumber ? this.output = this.output.replace('-', '') : this.output = `-${this.output}`;
     this.writeResult();
+  }
+
+  calculate() {
+    if(this.val1 == null) {
+      this.val1 = this.output;
+    } else if(this.val2 == null) {
+      if(this.operator == null) {
+        console.log('c');
+      } else {
+        if(this.output == null) {
+          this.val2 = this.val1;
+        } else {
+          this.val2 = this.output;
+        }
+        this.output = eval(`${this.val1} ${this.operator} ${this.val2}`).toString();
+        this.writeResult();
+        this.output = '0';
+      }
+    } else {
+      this.val1 = eval(`${this.val1} ${this.operator} ${this.val2}`).toString();
+      this.output = eval(`${this.val1} ${this.operator} ${this.val2}`).toString();
+      this.writeResult();
+      this.output = '0';
+    }
   }
 
   setOperator(operator, button) {
@@ -98,25 +124,25 @@ export class Calculator {
     if(this.val1 == null) {
       this.val1 = this.output;
       this.writeResult();
-      this.output = '0';
+      this.output = null;
     } else if(this.val2 == null) {
       this.val1 = eval(`${this.val1} ${this.operator} ${this.output}`);
       this.output = this.val1.toString();
       this.writeResult();
-      this.output = '0';
+      this.output = null;
     } else {
-      this.val1 = this.output;
+      this.val1 = eval(`${this.val1} ${this.operator} ${this.val2}`);
       this.val2 = null;
+      this.output = this.val1.toString();
       this.writeResult();
-      this.output = '0';
+      this.output = null;
     }
 
     this.operator = operator;
-    console.log('Operator set to: ' + operator);
-    console.log(`${this.val1} ${operator} ${this.val2}`)
   }
 
   updateOutput(value) {
+    this.output = this.output == null ? this.output = '0' : this.output;
     const valueLengthLimit = 9;
     const valueLength = this.output.replace('-','').replace(',','').length;
 
@@ -153,25 +179,29 @@ export class Calculator {
   }
 
   writeResult() {
+    console.log('Operator set to: ' + this.operator);
+    console.log(`${this.val1} ${this.operator} ${this.val2}`)
     this.output == "0" || this.output == "-0" ? this.UI.clear.textContent = "AC" : this.UI.clear.textContent = "C";
 
     const isComma = this.output.includes(',');
-    const intiger = this.output.split(',')[0];
+    const isMinus = this.output.includes('-');
+    const intiger = this.output.split(',')[0].replace('-','');
     let array = intiger.split("").reverse();
     let result = this.output;
 
+    //Add space every 3 intiger values
     if(intiger.length > 6) {
       array.splice(3,0," ");
       array.splice(7,0," ");
       result = array.reverse().join("");
       isComma ? result = `${result},${this.output.split(',')[1]}`: '';
-
+      result = isMinus ? `-${result}` : result;
       this.UI.result.textContent = result;
     } else if(intiger.length > 3) {
       array.splice(3,0," ");
       result = array.reverse().join("");
       isComma ? result = `${result},${this.output.split(',')[1]}`: '';
-
+      result = isMinus ? `-${result}` : result;
       this.UI.result.textContent = result;
     } else {
       this.UI.result.textContent = result;
